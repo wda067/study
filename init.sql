4c6477e89c8caaa4fcaae91f86c0a0cfbfb0f6f9
Jo Myeong-geun <happy63590@gmail.com>
2024-12-02 04:49:53 +0900

    chore: 데이터베이스 이름 수정

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 -- myuser에게 dailog 데이터베이스에 대한 모든 권한을 부여
-GRANT ALL PRIVILEGES ON study.* TO 'myuser'@'%';
+GRANT ALL PRIVILEGES ON mydb.* TO 'myuser'@'%';
 
 -- 권한 적용
 FLUSH PRIVILEGES;
 
-CREATE DATABASE study;
+CREATE DATABASE mydb;
