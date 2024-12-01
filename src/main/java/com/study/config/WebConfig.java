31fb7c1f717b05ba7fb7c5c6af8b24e202782994
Jo Myeong-geun <happy63590@gmail.com>
2024-12-02 04:15:02 +0900

    test deploy

--- src/main/java/com/study/config/WebConfig.java
+++ src/main/java/com/study/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.study.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +15,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/api/**")
                 .excludePathPatterns("/api/join", "/api/login");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**");
+            }
+        };
     }
 }
