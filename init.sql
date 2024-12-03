99f969a236a3bd789e8dd60ce6f3a74485364dba
Jo Myeong-geun <happy63590@gmail.com>
2024-12-04 07:12:46 +0900

    feat: 토스 페이먼츠 결제 기능 추가

--- init.sql
+++ init.sql
@@ -3,3 +3,16 @@ CREATE DATABASE mydb;
 GRANT ALL PRIVILEGES ON mydb.* TO 'myuser'@'%';
 
 FLUSH PRIVILEGES;
+
+CREATE TABLE payment (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         payment_key VARCHAR(255),
+                         order_id VARCHAR(255),
+                         order_name VARCHAR(255),
+                         method VARCHAR(255),
+                         total_amount BIGINT,
+                         member_id BIGINT,
+                         status VARCHAR(255),
+                         requested_at TIMESTAMP,
+                         FOREIGN KEY (member_id) REFERENCES member(id)
+);
